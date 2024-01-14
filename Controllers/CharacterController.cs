c893e4b348452ee9e8fc37a432952ff3067f8438
Kaan Tan <kaantan97@gmail.com>
2024-01-14 12:55:50 +0300

    New model and its controller is added. First API call has been made!! Hurrayy...

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,19 @@
+﻿using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static Character _knight = new Character();
+
+        [HttpGet]
+        public ActionResult<Character> Get()
+        {
+            return Ok(_knight);
+        }
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,31 @@
+﻿namespace dotnet_rpg.Models
+{
+    public class Character
+    {
+
+        public Character()
+        {
+            InitWithDefaults();
+        }
+
+        private void InitWithDefaults()
+        {
+            Name = "John Doe";
+            HitPoint = 100;
+            Strength = 10;
+            Defence = 10;
+            Intelligence = 10;
+            ClassType = RpgClass.Knight;
+        }
+
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int HitPoint { get; set; }
+        public int Strength { get; set; }
+        public int Defence { get; set; }
+        public int Intelligence { get; set; }
+        public RpgClass ClassType { get; set; }
+
+    }
+}
